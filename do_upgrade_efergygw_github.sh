8130f0276f3b95375c36afff07aaaff890ad3deb
Timo Koponen <hulttis@gmail.com>
2019-11-28 20:52:38 +0800

    3.3.8 (191128)

--- do_upgrade_efergygw_github.sh
+++ do_upgrade_efergygw_github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # copy to app directory (one level up from efergygw)
-echo efergygw github
+echo "efergygw (github)"
 systemctl stop efergygw
 rm -frv ./efergygw/new
 cp -v ./efergygw/efergygw.json ./efergygw.json.upg
@@ -9,3 +9,4 @@ cp -vr ./efergygw/new/* ./efergygw/.
 cp -vr efergygw.json.upg ./efergygw/efergygw.json
 rm -v efergygw.json.upg
 systemctl start efergygw
+echo "efegygw (github) upgrade done"
